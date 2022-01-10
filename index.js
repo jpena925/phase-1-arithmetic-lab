e7e6af18a573b52dd3556894b4bfadbff687b6c0
Jack Pena <penajackc@gmail.com>
2022-01-10 14:55:43 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+let num3 = 9
+let num4 = 5
+const mod = num3 % num4;
+
+const max = Math.max(1,5,6,7,8,20,17);
